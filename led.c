3778193e0e9f750e5c56b623abae57587c30c497
Jonathan Steele <jrsteele@gmail.com>
2013-03-10 07:59:07 -0400

    Altered code to correctly handling a 12 hour wait.

--- led.c
+++ led.c
@@ -1,23 +1,62 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-/* Set Delay in Milisecionds: 12 hours -> 43200000 */
+#ifdef DEBUG
+#define DEBUG_LED_PORT_DDR DDRC
+#define DEBUG_LED_PORT PORTC
+#define DEBUG_LED_PIN PC7
+#endif
+
+#define LED_PORT_DDR DDRD
+#define LED_PORT PORTD
+#define LED_PIN PD1
+
+/* Setup delays and compare constants */
 enum {
-  BLINK_DELAY_MS = 4320000UL,
+    BLINK_DELAY_MS = 1000,
+    TWELVE_HOURS_SEC = 43200
 };
 
 int main (void) {
-    /* set pin 2 of PORTD for output.*/
-    DDRD |= _BV(DDD1);
+    
+    /* Set the pin for output */
+    LED_PORT_DDR = (1 << LED_PIN);
 
+#ifdef DEBUG
+    /* Set the debug pin (led) for output */
+    DEBUG_LED_PORT_DDR = (1 << DEBUG_LED_PIN);
+#endif
+    
+    
+    /* Second counter */
+    static long counter = 0;
+    
+    /* Enable the port */
+    LED_PORT ^= (1 << LED_PIN);
+    
+#ifdef DEBUG
+    /* Enable the debug port */
+    DEBUG_LED_PORT ^= (1 << DEBUG_LED_PIN);
+#endif
+    
+    /* Main loop */
     while(1) {
-        /* set pin high to turn light on */
-        PORTD |= _BV(PORTD1);
+        
+        /* If the counter makes it to 12 hours, reset and change the pin */
+        if (counter == TWELVE_HOURS_SEC) {
+            counter = 0;
+            LED_PORT ^= (1 << LED_PIN);
+#ifdef DEBUG
+            DEBUG_LED_PORT ^= (1 << DEBUG_LED_PIN);
+#endif
+        }
+        
+        /* Increment */
+        counter++;
+        
+        /* Sleep for 1 second */
         _delay_ms(BLINK_DELAY_MS);
 
-        /* set pin low to turn light off */
-        PORTD &= ~_BV(PORTD1);
-        _delay_ms(BLINK_DELAY_MS);
     }
 
     return 0;
